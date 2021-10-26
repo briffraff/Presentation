21d7c6633e377e7d8ccd563dac5e807bf3533c5f
briffraff <briffraff6@gmail.com>
2021-10-27 01:04:35 +0300

    auto processing mannequins name object passed by their paths css classes for better thumbs fit

---
+++ files/js/nameProcessing.js
@@ -0,0 +1,185 @@
+let mFirst = [
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-1_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-1_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-4_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-4_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-8_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-8_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-9_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-9_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-10_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-10_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-11_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-11_Head_Low.OBJ',
+];
+
+let mSecond = [
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-4_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-4_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-5_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-5_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-6_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-6_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-7_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-7_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-9_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-9_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-10_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-11_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-12_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-12_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-Crossover_Head_HIgh.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-Crossover_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-FlyingDunk_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-FlyingDunk_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-JumpDefense_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-JumpDefense_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-Layup_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-Layup_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-RunDribble_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-RunDribble_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-Wheelchair_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-Wheelchair_Head_Low.OBJ',
+];
+
+let mThree = [
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-1_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-1_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-2_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-2_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-3_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-3_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-5_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-5_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-6_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-6_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-7_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-7_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-Run_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_GF-Run_Head_Low.OBJ',
+];
+
+let mFour = [
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MA-1_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MA-1_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MA-2_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MA-2_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MA-3_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MA-3_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MA-4_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MA-4_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MA-5_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MA-5_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MG-2_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_ML-1_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_ML-1_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_ML-2_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_ML-5_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_ML-5_Head_Low.OBJ',
+];
+
+let mFive = [
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_M-A-Pose_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_M-A-Pose_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_M-T-Pose_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_M-T-Pose_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_M-Relaxed_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_M-Relaxed_Head_Low.OBJ',
+];
+
+let mSix = [
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MAR-1_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MAR-1_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MAR-1-Mirrored_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MAR-1-Mirrored_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MAR-3_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MAR-3_Head_Low.obj',
+];
+
+let mSeven = [
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MB-1_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MB-1_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MB-2_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MB-2_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MB-2-3rd_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MB-2-3rd_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MB-PRO-B1_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MB-PRO-B1_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MG-2_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MLS-0_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MLS-0_Head_Low.OBJ',
+];
+
+let mEight = [
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MPS-Frank_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MPS-Frank_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MPS-Mike_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MPS-Mike_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MPS-MikeV2_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MPS-MikeV2_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MPS-R1_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MPS-R1_Head_Low.OBJ',
+];
+
+let mNine = [
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MT-Y01_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MT-Y01_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MY-0_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MY-0_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_PRO-2_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_PRO-2_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_SBM-2_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_SBM-2_Head_Low.OBJ',
+];
+
+let mLegs = [
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_GF-LegUp_LEG_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_GF-LegUp_LEG_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_GF-P3_LEG_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_GF-P3_LEG_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MA-P1_LEG_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MA-P1_LEG_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MRP-2_LEG_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MRP-2_LEG_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MS-1_LEG_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MS-1_LEG_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MF-ANK_LEG_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MF-ANK_LEG_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MF-CRW_LEG_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MF-CRW_LEG_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MF-OTC_LEG_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MF-OTC_LEG_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MF-OTK_LEG_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Legforms\\SM_MF-OTK_LEG_Low.OBJ',
+];
+
+
+function nameProcessing(pathList) {
+    let filterList = pathList
+        .sort((a, b) => a.localeCompare(b))
+        .filter(p => p.includes('Low') == false)
+        .map(x => {
+            let splitPath = x.split('\\');
+            let splitGeometry = splitPath[splitPath.length - 1];
+            let pose = splitGeometry.split('_')[1];
+
+            return [pose, 'mannieImg30'];
+        });
+
+    return filterList;
+}
+
+let mannequinsObject = {
+    'menCol1': nameProcessing(mFirst),
+    'menCol2': nameProcessing(mSecond),
+    'menCol3': nameProcessing(mThree),
+    'menCol4': nameProcessing(mFour),
+    'menCol5': nameProcessing(mFive),
+    'menCol6': nameProcessing(mSix),
+    'menCol7': nameProcessing(mSeven),
+    'menCol8': nameProcessing(mEight),
+    'menCol9': nameProcessing(mNine),
+    'men-legforms': nameProcessing(mLegs),
+};
+
+console.log(mannequinsObject);
--- files/js/slides.js
+++ files/js/slides.js
@@ -1,54 +1,102 @@
 function slides() {
 
     let mannequins = {
-        'first': [
-            ['AT-1', 'mannieImg'],
-            ['AT-4', 'mannieImg'],
-            ['AT-8', 'mannieImg'],
-            ['AT-9', 'mannieImg'],
-            ['AT-10', 'mannieImg'],
-            ['AT-11', 'mannieImg2'],
+        menCol1: [
+            ['AT-1', 'mannieImg30'],
+            ['AT-4', 'mannieImg30'],
+            ['AT-8', 'mannieImg30'],
+            ['AT-9', 'mannieImg30'],
+            ['AT-10', 'mannieImg30'],
+            ['AT-11', 'mannieImg40']
         ],
-        'second': [
-            ['MA-1', 'mannieImg'],
-            ['MA-2', 'mannieImg'],
-            ['MA-3', 'mannieImg'],
-            ['MA-4', 'mannieImg'],
-            ['MA-5', 'mannieImg'],
-            ['MA-P1', 'mannieImg'],
+        menCol2: [
+            ['BB-4', 'mannieImg35'],
+            ['BB-5', 'mannieImg40'],
+            ['BB-6', 'mannieImg50'],
+            ['BB-7', 'mannieImg30'],
+            ['BB-9', 'mannieImg30'],
+            ['BB-12', 'mannieImg30'],
+            ['BB-Crossover', 'mannieImg40'],
+            ['BB-FlyingDunk', 'mannieImg35'],
+            ['BB-JumpDefense', 'mannieImg35'],
+            ['BB-Layup', 'mannieImg30'],
+            ['BB-RunDribble', 'mannieImg40'],
+            ['BB-Wheelchair', 'mannieImg35']
         ],
-        'three': [
-            ['BB-4', 'mannieImg'],
-            ['BB-5', 'mannieImg2'],
-            ['BB-6', 'mannieImg3'],
-            ['BB-7', 'mannieImg'],
-            ['BB-9', 'mannieImg'],
-            ['BB-11', 'mannieImg'],
-            ['BB-12', 'mannieImg'],
+        menCol3: [
+            ['GF-1', 'mannieImg55'],
+            ['GF-2', 'mannieImg50'],
+            ['GF-3', 'mannieImg35'],
+            ['GF-5', 'mannieImg35'],
+            ['GF-6', 'mannieImg40'],
+            ['GF-7', 'mannieImg50'],
+            ['GF-Run', 'mannieImg30']
         ],
-        'four': [
-            ['GF-1', 'mannieImg4'],
-            ['GF-2', 'mannieImg3'],
-            ['GF-3', 'mannieImg'],
-            ['GF-5', 'mannieImg'],
-            ['GF-6', 'mannieImg2'],
-            ['GF-7', 'mannieImg3'],
-            ['GF-LegUp', 'mannieImg'],
+        menCol4: [
+            ['MA-1', 'mannieImg30'],
+            ['MA-2', 'mannieImg35'],
+            ['MA-3', 'mannieImg35'],
+            ['MA-4', 'mannieImg35'],
+            ['MA-5', 'mannieImg30'],
+            ['ML-1', 'mannieImg30'],
+            ['ML-2', 'mannieImg30'],
+            ['ML-5', 'mannieImg30']
+        ],
+        menCol5: [
+            ['M-A-Pose', 'mannieImg35'],
+            ['M-T-Pose', 'mannieImg35'],
+            ['M-Relaxed', 'mannieImg35']
+        ],
+        menCol6: [
+            ['MAR-1', 'mannieImg25'],
+            ['MAR-1-Mirrored', 'mannieImg25'],
+            ['MAR-3', 'mannieImg25']
+        ],
+        menCol7: [
+            ['MB-1', 'mannieImg35'],
+            ['MB-2', 'mannieImg35'],
+            ['MB-2-3rd', 'mannieImg35'],
+            ['MB-PRO-B1', 'mannieImg35'],
+            ['MLS-0', 'mannieImg35']
+        ],
+        menCol8: [
+            ['MPS-Frank', 'mannieImg35'],
+            ['MPS-Mike', 'mannieImg35'],
+            ['MPS-MikeV2', 'mannieImg35'],
+            ['MPS-R1', 'mannieImg35']
+        ],
+        menCol9: [
+            ['MG-2', 'mannieImg30'],
+            ['MT-Y01', 'mannieImg35'],
+            ['MY-0', 'mannieImg35'],
+            ['PRO-2', 'mannieImg35'],
+            ['SBM-2', 'mannieImg25']
+        ],
+        'men-legforms': [
+            ['GF-LegUp', 'mannieImg35'],
+            ['GF-P3', 'mannieImg35'],
+            ['MA-P1', 'mannieImg35'],
+            ['MRP-2', 'mannieImg35'],
+            ['MS-1', 'mannieImg35'],
+            ['MF-ANK', 'mannieImg25'],
+            ['MF-CRW', 'mannieImg25'],
+            ['MF-OTC', 'mannieImg25'],
+            ['MF-OTK', 'mannieImg30']
         ]
     };
 
     let config = {
-        "nikeLogo": "https://downloadwap.com/thumbs2/wallpapers/p2/2019/abstract/20/6550dcfb13097318.jpg",
-        "extension": ".png",
-        "imgRootPath": "files/img/",
+        'nikeLogo': 'https://downloadwap.com/thumbs2/wallpapers/p2/2019/abstract/20/6550dcfb13097318.jpg',
+        'extension': '.png',
+        'imgRootPath': 'files/img/',
     };
 
-    const slidesContainer = document.getElementsByClassName("slides")[0];
+    const slidesContainer = document.getElementsByClassName('slides')[0];
 
     function firstPage(nikeLogo) {
-        let slide = document.createElement("section");
-        let logo = document.createElement("img");
-        let text = document.createElement("h2");
+        let slide = document.createElement('section');
+        let logo = document.createElement('img');
+        let text = document.createElement('h2');
 
         logo.src = nikeLogo;
         logo.width = 300;
@@ -61,13 +109,13 @@ function slides() {
         slidesContainer.appendChild(slide);
 
         // // <section>
-        // //     <img src="https://downloadwap.com/thumbs2/wallpapers/p2/2019/abstract/20/6550dcfb13097318.jpg" alt="" width="300px" height="380px">
+        // //     <img src='https://downloadwap.com/thumbs2/wallpapers/p2/2019/abstract/20/6550dcfb13097318.jpg' alt='' width='300px' height='380px'>
         // //     <h2>NIKE MANNEQUINS MEN-[HEADS]</h2>
         // // </section>
-    }
+    };
 
     function createEmptySection(name) {
-        let slide = document.createElement("section");
+        let slide = document.createElement('section');
         slide.className = name;
 
         slidesContainer.appendChild(slide);
@@ -75,22 +123,22 @@ function slides() {
         // // <section>
         // // </section>
         return slide;
-    }
+    };
 
     function addMannequins(whereToAdd, manniesCollection) {
 
         for (const man of manniesCollection) {
-            [manniePose, imageSize] = man;
+            let [manniePose, imageSize] = man;
 
-            let mannie = document.createElement("section");
-            mannie.className = "mannie";
+            let mannie = document.createElement('section');
+            mannie.className = 'mannie';
 
-            let h1 = document.createElement("h1");
-            h1.className = "mannie-title";
+            let h1 = document.createElement('h1');
+            h1.className = 'mannie-title';
             h1.innerHTML = manniePose;
             mannie.appendChild(h1);
 
-            let img = document.createElement("img");
+            let img = document.createElement('img');
             img.className = imageSize;
             img.src = `${config.imgRootPath}${manniePose}${config.extension}`;
             mannie.appendChild(img);
@@ -98,11 +146,11 @@ function slides() {
             whereToAdd.appendChild(mannie);
         }
 
-        // // <section class="mannie">
-        // //     <h1 class="mannie-title">AT-1</h1>
-        // //     <img class="mannieImg" src="files/img/AT-1.png">
+        // // <section class='mannie'>
+        // //     <h1 class='mannie-title'>AT-1</h1>
+        // //     <img class='mannieImg35' src='files/img/AT-1.png'>
         // // </section>
-    }
+    };
 
 
     firstPage(config.nikeLogo);
@@ -111,5 +159,4 @@ function slides() {
         let emptySection = createEmptySection(container);
         addMannequins(emptySection, mannequins[container]);
     }
-}
-
+};
