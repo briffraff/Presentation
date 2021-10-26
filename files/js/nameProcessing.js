84d95eab637ca943467ec3eee1686a751ddc0aec
briffraff <briffraff6@gmail.com>
2021-10-27 02:27:36 +0300

    update mannequin names list, should fix commented ones

--- files/js/nameProcessing.js
+++ files/js/nameProcessing.js
@@ -1,3 +1,7 @@
+
+/////   IF OBJECT IS NOT CORRECT KEY REPLACE \ WITH \\
+
+//  MEN
 let mFirst = [
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-1_Head_High.OBJ',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_AT-1_Head_Low.OBJ',
@@ -24,7 +28,9 @@ let mSecond = [
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-7_Head_Low.OBJ',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-9_Head_High.obj',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-9_Head_Low.OBJ',
+    // 'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-10_Head_High.OBJ',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-10_Head_Low.OBJ',
+    // 'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-11_Head_High.OBJ',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-11_Head_Low.OBJ',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-12_Head_High.obj',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_BB-12_Head_Low.OBJ',
@@ -70,9 +76,9 @@ let mFour = [
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MA-4_Head_Low.OBJ',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MA-5_Head_High.obj',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MA-5_Head_Low.OBJ',
-    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MG-2_Head_Low.OBJ',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_ML-1_Head_High.obj',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_ML-1_Head_Low.obj',
+    // 'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_ML-2_Head_High.obj',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_ML-2_Head_Low.obj',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_ML-5_Head_High.OBJ',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_ML-5_Head_Low.OBJ',
@@ -106,6 +112,7 @@ let mSeven = [
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MB-PRO-B1_Head_High.OBJ',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MB-PRO-B1_Head_Low.OBJ',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MG-2_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MG-2_Head_Low.OBJ',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MLS-0_Head_High.obj',
     'N:\\Avatars\\Mannequins Morphable\\Mens\\Models_WIth_Heads\\SM_MLS-0_Head_Low.OBJ',
 ];
@@ -154,6 +161,183 @@ let mLegs = [
 ];
 
 
+//  YOUNG ATHLETES
+let yaFirst = [
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-6_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-6_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-10_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-10_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-12_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-12_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-A-Pose_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-A-Pose_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-B1_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-B1_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-G2_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-G2_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_LPS-YAG15_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_LPS-YAG15_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-1_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-1_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-2_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAG-2_Head_Low.obj'
+];
+
+let yaSecond = [
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_FPS-YAB19_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_FPS-YAB19_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-1_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-1_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-2_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-2_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-8_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-8_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-12_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-12_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-14_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-14_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-15_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-15_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-A-Pose_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-A-Pose_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-B1_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-B1_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-Clo_Head_High.obj',
+    // 'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-Clo_Head_Low.obj',
+    // 'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-SCh_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Models_With_Heads\\SM_YAB-SCh_Head_Low.obj',
+];
+
+let yaLegs = [
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Legforms\\SM_YAB-S1_LEG_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Young Athletes\\Legforms\\SM_YAB-S1_LEG_Low.OBJ'
+];
+
+
+//  WOMEN
+let wFirst = [
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WA-1_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WA-1_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WA-2_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WA-2_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WA-3_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WA-3_Heads_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WA-4_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WA-4_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-A-FIT_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-A-FIT_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-A-FIT2_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-A-FIT2_Head_Low.OBJ',
+];
+
+let wSecond = [
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-A-Pose_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-A-Pose_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-A-PoseNaturalGait_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-A-PoseNaturalGait_Head_Low.OBJ',
+];
+
+let wThree = [
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WAR-1_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WAR-1_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WAR-2_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WAR-2_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WAR-2-Mirrored_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WAR-2-Mirrored_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WAR-3_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WAR-3_Head_Low.OBJ',
+];
+
+let wFour = [
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WA-Relaxed_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WA-Relaxed_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-Relaxed_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-Relaxed_Head_Low.OBJ',
+    // 'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-Relaxed2_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-Relaxed2_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-RO_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-RO_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WB-1_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WB-1_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WB-2_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WB-2_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WC-1_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WC-1_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-HP_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-HP_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-KB_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_W-KB_Head_Low.OBJ',
+];
+
+let wFive = [
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_EMA-0_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_EMA-0_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WL-2_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WL-2_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WL-2-Mirrored_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WL-2-Mirrored_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WL-3_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WL-3_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WL-4_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WL-4_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WL-5_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WL-5_Head_Low.OBJ',
+];
+
+let wSix = [
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WPS-Jane_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WPS-Jane_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WPS-Jane-P_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WPS-Jane-P_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WPS-Jane-P2_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WPS-Jane-P2_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WPS-Monica_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WPS-Monica_Head_Low.OBJ',
+];
+
+let wSeven = [
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-1B_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-1B_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-2A_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-2A_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-2B_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-2B_Head_Low.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-3A_Head_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-3A_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-5A_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-5A_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-6_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-6_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-7_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-7_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-9_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-9_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-10_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-10_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-B5_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-B5_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-H_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-H_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-Y01_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-Y01_Head_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-YogaCamel2_Head_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Models_With_Heads\\SM_WT-YogaCamel2_Head_Low.OBJ',
+];
+
+let wLegs = [
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Legforms\\SM_WP-1_LEG_High.obj',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Legforms\\SM_WP-1_LEG_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Legforms\\SM_WP-R_LEG_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Legforms\\SM_WP-R_LEG_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Legforms\\SM_WP-R3_LEG_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Legforms\\SM_WP-R3_LEG_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Legforms\\SM_WS-1_LEG_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Legforms\\SM_WS-1_LEG_Low.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Legforms\\SM_WT-P2_LEG_High.OBJ',
+    'N:\\Avatars\\Mannequins Morphable\\Womens\\Legforms\\SM_WT-P2_LEG_Low.OBJ',
+];
+
+
 function nameProcessing(pathList) {
     let filterList = pathList
         .sort((a, b) => a.localeCompare(b))
@@ -169,7 +353,13 @@ function nameProcessing(pathList) {
     return filterList;
 }
 
-let mannequinsObject = {
+let yaObject = {
+    'yaCol1': nameProcessing(yaFirst),
+    'yaCol2': nameProcessing(yaSecond),
+    'ya-legforms': nameProcessing(yaLegs),
+};
+
+let menObject = {
     'menCol1': nameProcessing(mFirst),
     'menCol2': nameProcessing(mSecond),
     'menCol3': nameProcessing(mThree),
@@ -182,4 +372,17 @@ let mannequinsObject = {
     'men-legforms': nameProcessing(mLegs),
 };
 
-console.log(mannequinsObject);
+let womenObject = {
+    'womenCol1': nameProcessing(wFirst),
+    'womenCol2': nameProcessing(wSecond),
+    'womenCol3': nameProcessing(wThree),
+    'womenCol4': nameProcessing(wFour),
+    'womenCol5': nameProcessing(wFive),
+    'womenCol6': nameProcessing(wSix),
+    'womenCol7': nameProcessing(wSeven),
+    'women-legforms': nameProcessing(wLegs),
+};
+
+console.log(womenObject);
+
+////////////////////   IF OBJECT IS NOT CORRECT KEY REPLACE \ WITH \\
